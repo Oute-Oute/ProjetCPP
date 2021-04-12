380b5539303693ca4068b3a2a78811b7491af8c9
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-04-12 15:50:04 +0200

    opérations
    opérations finies en théorie, gestion des exceptions commencée (déja envie de mouwiw...)

--- ProjetCPPMatrice/ProjetCPPMatrice/CException.h
+++ ProjetCPPMatrice/ProjetCPPMatrice/CException.h
@@ -3,10 +3,21 @@
 
 #include <fstream>
 #include <iostream>
+#include <cstring>
 
 class CException
 {
+private:
+	const char* sEXCMessage;
+
 public:
+	//constructeur a partir d un message d exception
+	CException(const char* sMessage);
+
+	~CException();
+
+	char* getMessage();
+
 	int EXCAddition();
 
 	int EXCSoustraction();
